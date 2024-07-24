5c5f7521d047a61127e272303364cbd0b2dc1747
matheus <viniciusomagnifico@hotmail.com>
2024-07-23 22:26:00 -0300

    atualização de header

--- src/main.js
+++ src/main.js
@@ -3,7 +3,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
     const questions = document.querySelectorAll('[data-faq-question')
 
-    
+    const herosection = document.querySelector('.hero');
+    const alturaHero = herosection.clientHeight
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader(); 
+        }else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    function ocultaElementosDoHeader() {
+        const header = document.querySelector('header')
+        header.classList.add('header--is-header');
+    }
+
+
+    function exibeElementosDoHeader() {
+        const header = document.querySelector('header')
+        header.classList.remove('header--is-header');
+    }
+
+   //seção de atrções, programação das abas 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
