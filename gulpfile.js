d90d35c190de58c053cfca290959d6200e60012c
matheus <viniciusomagnifico@hotmail.com>
2024-07-25 23:07:02 -0300

    ajuste

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,6 @@ function styles() {
 }
 
 
-function images() {
-    return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
-}
-
 exports.default = gulp.parallel(styles, images);
 
 exports.watch = function() {
